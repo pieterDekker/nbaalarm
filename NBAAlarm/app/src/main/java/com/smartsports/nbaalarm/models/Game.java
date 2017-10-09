93a625c600ce95c1ebd565caf5360cda816ed696
pieterDekker <pieter.dekker20@gmail.com>
2017-10-09 22:19:02 +0200

    in models/Game.java: removed commented code, removed serializable interface

--- NBAAlarm/app/src/main/java/com/smartsports/nbaalarm/models/Game.java
+++ NBAAlarm/app/src/main/java/com/smartsports/nbaalarm/models/Game.java
@@ -1,13 +1,12 @@
 package com.smartsports.nbaalarm.models;
 
-import java.io.Serializable;
 import java.util.Date;
 
 /**
  * Created by pieter on 22-9-17.
  */
 
-public class Game implements Serializable {
+public class Game {
     private String team_1;
 
     private String team_2;
@@ -28,23 +27,12 @@ public class Game implements Serializable {
         return team_1;
     }
 
-//    public void setTeam_1(String team_1) {
-//        this.team_1 = team_1;
-//    }
-
     public String getTeam_2() {
         return team_2;
     }
-
-//    public void setTeam_2(String team_2) {
-//        this.team_2 = team_2;
-//    }
 
     public Date getStart() {
         return start;
     }
 
-//    public void setStart(Date start) {
-//        this.start = start;
-//    }
 }
